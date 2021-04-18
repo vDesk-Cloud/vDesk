7fec14007b906cde8588ee1ee37c0e06656dcbab
Kerry <DevelopmentHero@gmail.com>
2021-04-18 04:40:34 +0200

    -Raised version number.

--- Server/Lib/vDesk/Packages/Pages.php
+++ Server/Lib/vDesk/Packages/Pages.php
@@ -27,7 +27,7 @@ final class Pages extends Package implements IPackage {
     /**
      * The version of the Package.
      */
-    public const Version = "1.0.2";
+    public const Version = "1.1.1";
     
     /**
      * The name of the Package.
