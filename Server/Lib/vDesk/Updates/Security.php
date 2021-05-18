4e405688f25bd8d6d2ccdeab1861d1a82f407d5b
Kerry <DevelopmentHero@gmail.com>
2021-05-18 12:37:30 +0200

    -Added update manifest.

---
+++ Server/Lib/vDesk/Updates/Security.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+namespace vDesk\Updates;
+
+use vDesk\Packages\Package;
+
+/**
+ * Security Update manifest class.
+ *
+ * @package vDesk
+ * @author  Kerry <DevelopmentHero@gmail.com>
+ */
+class Security extends Update {
+
+    /**
+     * The class name of the Package of the Update.
+     */
+    public const Package = \vDesk\Packages\Security::class;
+
+    /**
+     * The required Package version of the Update.
+     */
+    public const RequiredVersion = "1.0.0";
+
+    /**
+     * The description of the Update.
+     */
+    public const Description = <<<Description
+- Added option for logging out specific Users.
+- Added support for preserving the current logged in User while performing multiple logins.
+Description;
+
+    /**
+     * The files and directories of the Update.
+     */
+    public const Files = [
+        self::Deploy   => [
+            Package::Server => [
+                Package::Modules => [
+                    "Security.php"
+                ],
+                Package::Lib => [
+                    "Security/User.php"
+                ]
+            ]
+        ],
+        self::Undeploy => [
+            Package::Server => [
+                Package::Modules => [
+                    "Security.php"
+                ],
+                Package::Lib     => [
+                    "Security/User.php"
+                ]
+            ]
+        ]
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public static function Install(\Phar $Phar, string $Path): void {
+        self::Undeploy();
+        self::Deploy($Phar, $Path);
+    }
+}
