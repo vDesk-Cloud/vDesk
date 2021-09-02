92ca86065a0e342d9626a7c839847bdb053c77d5
Kerry <DevelopmentHero@gmail.com>
2021-09-02 21:39:47 +0200

    -Updated commentary. -Updated formatting.

--- Server/Lib/vDesk/Data/ICollectionModel.php
+++ Server/Lib/vDesk/Data/ICollectionModel.php
@@ -6,19 +6,18 @@ namespace vDesk\Data;
 use vDesk\Struct\Collections\ICollection;
 
 /**
- * Represents a database model that contains a Collection of {@link \vDesk\Data\IModel} Models,
- * providing mechanisms for common CRUD operations.
+ * Represents a database model that contains a Collection of IModels, providing mechanisms for common CRUD operations.
  *
- * @package vDesk\Data
+ * @package vDesk
  * @author  Kerry Holz <DevelopmentHero@gmail.com>
  */
 interface ICollectionModel extends ICollection, IModel {
-    
+
     /**
      * Initializes a new instance of the ICollectionModel class.
      *
      * @param iterable $Models Initializes the ICollectionModel with the specified set of Models.
      */
     public function __construct(iterable $Models = []);
-    
+
 }
