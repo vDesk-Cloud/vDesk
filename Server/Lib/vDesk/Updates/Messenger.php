76db0bc55f4e441c3c3f859bae3474b2fc3e9038
Kerry <DevelopmentHero@gmail.com>
2021-10-18 09:17:42 +0200

    Typo.

--- Server/Lib/vDesk/Updates/Messenger.php
+++ Server/Lib/vDesk/Updates/Messenger.php
@@ -19,7 +19,7 @@ final class Messenger extends Update {
     /**
      * The required version of the Update.
      */
-    public const RequiredVersion = "1.0.1";
+    public const RequiredVersion = "1.0.0";
     
     /**
      * The description of the Update.
