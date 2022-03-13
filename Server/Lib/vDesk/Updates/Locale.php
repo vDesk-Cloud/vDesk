b174d16564e234a4f19d08767324cbdc4dd4b5de
Kerry <DevelopmentHero@gmail.com>
2022-03-13 23:15:23 +0100

    -Reworked Locale client files for vDesk-1.1.0

--- Server/Lib/vDesk/Updates/Locale.php
+++ Server/Lib/vDesk/Updates/Locale.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace vDesk\Updates;
 
+use vDesk\Packages\Package;
+
 /**
  * Locale Update manifest class.
  *
@@ -15,23 +17,45 @@ class Locale extends Update {
      * The class name of the Package of the Update.
      */
     public const Package = \vDesk\Packages\Locale::class;
-    
+
     /**
      * The required Package version of the Update.
      */
     public const RequiredVersion = "1.0.1";
-    
+
     /**
      * The description of the Update.
      */
     public const Description = <<<Description
-- Added compatibility to DataProvider-1.0.0.
+- Added compatibility to vDesk-1.1.0.
 Description;
+
+    /**
+     * The files and directories of the Update.
+     */
+    public const Files = [
+        self::Deploy   => [
+            Package::Client => [
+                Package::Lib => [
+                    "vDesk/Locale.js"
+                ]
+            ]
+        ],
+        self::Undeploy => [
+            Package::Client => [
+                Package::Lib => [
+                    "vDesk/Locale.js"
+                ]
+            ]
+        ]
+    ];
 
     /**
      * @inheritDoc
      */
     public static function Install(\Phar $Phar, string $Path): void {
-        //Just update Package manifest.
+        //Update files.
+        self::Undeploy();
+        self::Deploy($Phar, $Path);
     }
 }
