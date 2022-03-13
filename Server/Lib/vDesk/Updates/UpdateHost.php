70b0d28c60955a04e69360b438f4bf79d23d7d4d
Kerry <DevelopmentHero@gmail.com>
2022-03-13 23:05:01 +0100

    -Reworked Updates files for vDesk-1.1.0

--- Server/Lib/vDesk/Updates/UpdateHost.php
+++ Server/Lib/vDesk/Updates/UpdateHost.php
@@ -35,6 +35,11 @@ Description;
      */
     public const Files = [
         self::Deploy   => [
+            Package::Client => [
+                Package::Lib => [
+                    "vDesk/UpdateHost.js"
+                ]
+            ],
             Package::Server => [
                 Package::Modules => [
                     "UpdateHost.php"
@@ -42,6 +47,11 @@ Description;
             ]
         ],
         self::Undeploy => [
+            Package::Client => [
+                Package::Lib => [
+                    "vDesk/UpdateHost.js"
+                ]
+            ],
             Package::Server => [
                 Package::Modules => [
                     "UpdateHost.php"
