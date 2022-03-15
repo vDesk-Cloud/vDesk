0ff77ed34892eea6fd6ce40a0095049dc974a7a3
Kerry <DevelopmentHero@gmail.com>
2022-03-15 09:12:47 +0100

    -Fixed bugg with escaping too many characters.

---
+++ Server/Lib/vDesk/Updates/DataProvider.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace vDesk\Updates;
+
+use vDesk\Packages\Package;
+
+/**
+ * Contacts Update manifest class.
+ *
+ * @package vDesk\Contacts
+ * @author  Kerry <DevelopmentHero@gmail.com>
+ */
+final class DataProvider extends Update {
+
+    /**
+     * The Package of the Update.
+     */
+    public const Package = \vDesk\Packages\DataProvider::class;
+
+    /**
+     * The required Package version of the Update.
+     */
+    public const RequiredVersion = "1.0.0";
+
+    /**
+     * The description of the Update.
+     */
+    public const Description = <<<Description
+- Reduced amount of escaped characters in strings of MsSQL-DataProvider.
+Description;
+
+    /**
+     * The files and directories of the Update.
+     */
+    public const Files = [
+        self::Deploy   => [
+            Package::Server => [
+                Package::Lib => [
+                    "vDesk/DataProvider/MsSQL/Provider.php"
+                ]
+            ]
+        ],
+        self::Undeploy => [
+            Package::Server => [
+                Package::Lib => [
+                    "vDesk/DataProvider/MsSQL/Provider.php"
+                ]
+            ]
+        ]
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public static function Install(\Phar $Phar, string $Path): void {
+        //Update files.
+        self::Undeploy();
+        self::Deploy($Phar, $Path);
+    }
+}
