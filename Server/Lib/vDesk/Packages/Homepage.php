1ebed82a8845b7d6348746dda996ebc235ed389d
Kerry <DevelopmentHero@gmail.com>
2022-03-21 03:25:17 +0100

    -Fixed fullscreen display of images.

--- Server/Lib/vDesk/Packages/Homepage.php
+++ Server/Lib/vDesk/Packages/Homepage.php
@@ -22,7 +22,7 @@ class Homepage extends Package implements IPackage {
     /**
      * The version of the Package.
      */
-    public const Version = "1.0.1";
+    public const Version = "1.0.2";
     
     /**
      * The vendor of the Package.
