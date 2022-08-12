894f0341094b59a2d0862a2af529fee3c0156d07
Kerry <DevelopmentHero@gmail.com>
2022-08-12 13:00:49 +0200

    -Updated Security documentation.

--- Server/Templates/Documentation/Security.php
+++ Server/Templates/Documentation/Security.php
@@ -43,6 +43,10 @@ use vDesk\Pages\Functions;
         <h3>Concepts</h3>
         <p>
             Security is managed through groups for global rights and "Access Control Lists" for single elements.
+
+        </p>
+        <p>
+            In general, the permissions follow the principle of "permission over prohibition", this means if a user has
         </p>
     </section>
     <section id="Users">
@@ -50,8 +54,39 @@ use vDesk\Pages\Functions;
     </section>
     <section id="Groups">
         <h3>Groups</h3>
+        <p>
+            Groups are dictionaries of global permissions
+        </p>
+        <aside onclick="this.classList.toggle('Fullscreen')">
+            <img src="<?= Functions::Image("Documentation/Groups.png") ?>" alt="Image showing the administrative view of the group- and rights management">
+        </aside>
+    </section>
+    <section id="GroupPermissions">
+        <h4>Adding/removing Group permissions</h4>
+        <p>
+            The permissions of a group are defined over the database where each column of the <code class="Inline"><?= Code::Const("Security") ?>.<?= Code::Field("Groups") ?></code>-table
+            represents a certain permission.
+        </p>
+        <p>
+            Permissions can be created or deleted by calling the Security Module's <code class="Inline"><?= Code::Class("Security") ?>::<?= Code::Function("CreatePermission") ?>(<?= Code::Keyword("string") ?> <?= Code::Variable("\$Name") ?>, <?= Code::Keyword("bool") ?> <?= Code::Variable("\$Default") ?>)</code>- and
+            <code class="Inline"><?= Code::Class("Security") ?>::<?= Code::Function("DeletePermission") ?>(<?= Code::Keyword("string") ?> <?= Code::Variable("\$Name") ?>)</code>-methods.
+        </p>
+        <p>
+            Both operations require the current user to be a member of a group which has the "UpdateGroup"-permission been granted.
+        </p>
+        <aside class="Note">
+            <h4>Note</h4>
+            <p>
+                The default value of each permission is used as an initial reference while creating the "Everyone"-group on setup;
+                after that the group's permissions will be used as a template for creating new groups.<br>
+                So keep in mind setting the default permissions in the perspective of a normal user.
+            </p>
+        </aside>
     </section>
     <section id="ACL">
         <h3>Access Control Lists</h3>
+        <p>
+            "Access Control Lists" or "ACLs" are collections of individual permissions on certain "access controlled" entities.
+        </p>
     </section>
 </article>
