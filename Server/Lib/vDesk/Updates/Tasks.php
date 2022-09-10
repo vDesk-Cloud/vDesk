993d399b1d0368100ff8b655ca23789c42478c90
Kerry <DevelopmentHero@gmail.com>
2022-09-10 22:58:43 +0200

    -Updated Update manifest.

--- Server/Lib/vDesk/Updates/Tasks.php
+++ Server/Lib/vDesk/Updates/Tasks.php
@@ -13,57 +13,61 @@ use vDesk\Packages\Package;
  * @author  Kerry <DevelopmentHero@gmail.com>
  */
 class Tasks extends Update {
-    
+
     /**
      * The Package of the Update.
      */
     public const Package = \vDesk\Packages\Tasks::class;
-    
+
     /**
      * The required version of the Update.
      */
     public const RequiredVersion = "1.0.0";
-    
+
     /**
      * The description of the Update.
      */
     public const Description = <<<Description
 - Added support for scheduling intervals in microseconds.
-- Implemented machine like flow control callbacks for Tasks.
+- Updated tasks to implement machine like flow control callbacks.
+- Added anonymous inline tasks.
+- Added promises for task based asynchronous programming.
 Description;
-    
+
     /**
      * The files and directories of the Update.
      */
     public const Files = [
-        self::Undeploy   => [
+        self::Undeploy => [
             Package::Server => [
-                Package::Lib     => [
+                Package::Lib => [
+                    "vDesk/Tasks/Task.php",
                     "vDesk/Tasks/Tasks.php"
                 ]
             ]
         ],
         self::Deploy   => [
             Package::Server => [
-                Package::Lib     => [
-                    "vDesk/Tasks/Tasks.php"
+                Package::Lib => [
+                    "vDesk/Tasks/Task.php",
+                    "vDesk/Tasks/Tasks.php",
+                    "vDesk/Tasks/Promise.php",
+                    "vDesk/Tasks/Inline/Task.php",
                 ]
             ]
         ]
     ];
-    
-    /**
-     * @inheritDoc
-     */
+
+    /** @inheritDoc */
     public static function Install(\Phar $Phar, string $Path): void {
         //Uninstall old Machines.
         $Package = new \vDesk\Packages\Tasks();
         Modules::Machines()::Uninstall($Package);
-        
+
         //Patch files.
         self::Undeploy();
         self::Deploy($Phar, $Path);
-        
+
         //Install new Machines.
         Modules::Machines()::Install($Package);
     }
